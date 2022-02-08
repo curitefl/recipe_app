6e326c388dc65fa70ffbe0ba283b4fd165fe48d5
curite <onthepound@gmail.com>
2022-02-08 10:43:54 +0900

    primarySwatchを黄色に変更

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'レシピアプリ',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
       home: const MyPage('マイページ'),
     );
