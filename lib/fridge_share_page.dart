e83baaa28cbd1788eaef43012797067c3e6347dd
curite <onthepound@gmail.com>
2022-02-08 18:16:46 +0900

    サブページのクラスを作成

---
+++ lib/fridge_share_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FridgeSharePage extends StatelessWidget{
+  final String title;
+  const FridgeSharePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(title),
+      ),
+    );
+  }
+}
---
+++ lib/settings_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget{
+  final String title;
+  const SettingsPage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(title),
+      ),
+    );
+  }
+}
