6a2a30552cd2c83a10ab6c3156b50eaa2ffbce33
curite <onthepound@gmail.com>
2022-02-08 21:53:49 +0900

    お気に入りレシピのページにタブバーを追加

--- lib/sub_pages/favorite_recipe_page.dart
+++ lib/sub_pages/favorite_recipe_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class FavoriteRecipePage extends StatelessWidget{
+class FavoriteRecipePage extends StatelessWidget {
   final String title;
+
   const FavoriteRecipePage({Key? key, required this.title}) : super(key: key);
 
   @override
@@ -11,6 +12,18 @@ class FavoriteRecipePage extends StatelessWidget{
       appBar: AppBar(
         centerTitle: true,
         title: Text(title),
+      ),
+      body: const DefaultTabController(
+        length: 5,
+        child: TabBar(
+          tabs: [
+            Tab(text: '主菜'),
+            Tab(text: '副菜'),
+            Tab(text: '汁物'),
+            Tab(text: 'ご飯物'),
+            Tab(text: 'デザート'),
+          ],
+        ),
       ),
     );
   }
