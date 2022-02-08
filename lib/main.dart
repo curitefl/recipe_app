f869c384468f0f246c55ec32d32d455aac35e17d
curite <onthepound@gmail.com>
2022-02-08 17:44:26 +0900

    画面遷移機能を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/favorite_recipe_page.dart';
 import 'package:recipe_app/my_page.dart';
 import 'package:recipe_app/text_data.dart';
 
@@ -17,6 +18,14 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.amber,
       ),
       home: MyPage(TextData.myPage),
+      routes: <String, WidgetBuilder> {
+        '/お気に入りレシピ': (BuildContext context) => const FavoriteRecipePage(title: 'お気に入りレシピ'),
+        '/コラム': (BuildContext context) => const FavoriteRecipePage(title: 'コラム'),
+        '/プロフィール変更': (BuildContext context) => const FavoriteRecipePage(title: 'プロフィール変更'),
+        '/メールアドレス登録': (BuildContext context) => const FavoriteRecipePage(title: 'メールアドレス登録'),
+        '/冷蔵庫共有': (BuildContext context) => const FavoriteRecipePage(title: '冷蔵庫共有'),
+        '/設定': (BuildContext context) => const FavoriteRecipePage(title: '設定'),
+      },
     );
   }
 }
