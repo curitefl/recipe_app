a98aa73f1b5cf254d7b9d177d3ea5c2bf327d9d0
curite <onthepound@gmail.com>
2022-02-12 18:16:31 +0900

    配列の要素の数が変更されていたら、エラーが出るようにしました。

--- lib/sub_pages/profile_edit_page_model.dart
+++ lib/sub_pages/profile_edit_page_model.dart
@@ -20,6 +20,9 @@ class ProfileEditPageModel extends ChangeNotifier {
   }
 
   void selectDateOfBirth(DateTime dateOfBirth) {
+    assert(initDateOfBirth.length == 3,
+      'initDateOfBirthリストの要素数が変更されています（要素は3つでなければいけません）');
+
     initDateOfBirth[0] = dateOfBirth.year;
     initDateOfBirth[1] = dateOfBirth.month;
     initDateOfBirth[2] = dateOfBirth.day;
