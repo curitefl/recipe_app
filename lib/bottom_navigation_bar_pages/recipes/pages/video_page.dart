ebd107ca93f66443843f6295b24f32136ec0cfac
curite <onthepound@gmail.com>
2022-03-23 16:31:10 +0900

    レシピをクリックすると、動画のタイトルと、概要欄の説明が表示されるよう対応

--- lib/bottom_navigation_bar_pages/recipes/pages/video_page.dart
+++ lib/bottom_navigation_bar_pages/recipes/pages/video_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:provider/provider.dart';
-import 'package:recipe_app/bottom_navigation_bar_pages/recipes/pages/video_page_model.dart';
-import 'package:recipe_app/bottom_navigation_bar_pages/recipes/youtube_album.dart';
+import 'package:recipe_app/video_arguments.dart';
 
 class VideoPage extends StatelessWidget {
   final String title;
@@ -11,6 +9,7 @@ class VideoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as VideoArguments;
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(50.0.h),
@@ -21,30 +20,24 @@ class VideoPage extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Consumer<VideoPageModel>(builder: (context, model, child) {
-          return FutureBuilder<Album>(
-            future: model.futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView(children: [
-                  Text(
-                    snapshot.data!.title,
-                    style: Theme.of(context).textTheme.headline2,
-                  ),
-                  SizedBox(height: 24.0.h),
-                  Text(
-                    snapshot.data!.description,
-                    style: Theme.of(context).textTheme.headline2,
-                  ),
-                ]);
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          );
-        }),
+        child: ListView(
+          children: [
+            Image.network(
+              args.youtubeVideo.thumbnail.medium.url ?? '',
+              width: 120.0.w,
+            ),
+            SizedBox(height: 24.0.h),
+            Text(
+              args.youtubeVideo.title,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            SizedBox(height: 24.0.h),
+            Text(
+              args.youtubeVideo.description.toString(),
+              style: Theme.of(context).textTheme.headline2,
+            ),
+          ],
+        ),
       ),
     );
   }
