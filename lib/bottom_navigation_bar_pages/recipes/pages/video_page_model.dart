12b1acd152a4f1208653325af97b83c7fbad867e
curite <onthepound@gmail.com>
2022-03-24 10:19:28 +0900

    YoutubePlayerのコントローラーを作成

--- lib/bottom_navigation_bar_pages/recipes/pages/video_page_model.dart
+++ lib/bottom_navigation_bar_pages/recipes/pages/video_page_model.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoPageModel extends ChangeNotifier {
-
+  final YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: 'iLnmTe5Q2Qw',
+    flags: const YoutubePlayerFlags(
+      autoPlay: true,
+      mute: true,
+    ),
+  );
 }
