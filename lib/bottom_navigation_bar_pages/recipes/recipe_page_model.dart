7bb27a3a35bc6094b3b1187fe853a790712dbc1a
curite <onthepound@gmail.com>
2022-03-24 16:33:50 +0900

    変数をprivateに変更。_videoResultは、recipe_page.dartで参照したいのでゲッターを作成

--- lib/bottom_navigation_bar_pages/recipes/recipe_page_model.dart
+++ lib/bottom_navigation_bar_pages/recipes/recipe_page_model.dart
@@ -3,12 +3,14 @@ import 'package:recipe_app/text_data.dart';
 import 'package:recipe_app/youtube_api.dart';
 
 class RecipePageModel extends ChangeNotifier {
-  YoutubeAPI youtube = YoutubeAPI(TextData.youtubeV3APIKey);
-  List<YouTubeVideo> videoResult = [];
+  final YoutubeAPI _youtube = YoutubeAPI(TextData.youtubeV3APIKey);
+  List<YouTubeVideo> _videoResult = [];
+
+  List<YouTubeVideo> get videoResult => _videoResult;
 
   Future<void> callAPI() async {
     String query = TextData.queryKeyword;
-    videoResult = await youtube.search(
+    _videoResult = await _youtube.search(
       query,
       order: TextData.relevance,
       videoDuration: TextData.any,
