4f54b8f9c5e47efd3289e65565fa886ec5ec7227
curite <onthepound@gmail.com>
2022-03-25 15:03:23 +0900

    フルスクリーンにしてもAppBarが上部に表示されたままになってしまう問題を修正

--- lib/bottom_navigation_bar_pages/recipes/pages/video_page.dart
+++ lib/bottom_navigation_bar_pages/recipes/pages/video_page.dart
@@ -13,50 +13,50 @@ class VideoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as VideoArguments;
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(50.0.h),
-        child: AppBar(
-          centerTitle: true,
-          title: Text(title),
-          titleTextStyle: Theme.of(context).textTheme.headline3,
-        ),
-      ),
-      body: Center(
-        child: ListView(
-          children: [
-            Consumer<VideoPageModel>(builder: (context, model, child) {
-              return YoutubePlayerBuilder(
-                player: YoutubePlayer(
-                  controller: model.initVideoID(args.youtubeVideo.id.toString()),
-                  showVideoProgressIndicator: true,
-                  progressIndicatorColor: Colors.amber,
-                  progressColors: const ProgressBarColors(
-                    playedColor: Colors.amber,
-                    handleColor: Colors.amberAccent,
+    return Consumer<VideoPageModel>(
+      builder: (context, model, child) {
+        return YoutubePlayerBuilder(
+          player: YoutubePlayer(
+            controller: model.initVideoID(args.youtubeVideo.id.toString()),
+            showVideoProgressIndicator: true,
+            progressIndicatorColor: Colors.amber,
+            progressColors: const ProgressBarColors(
+              playedColor: Colors.amber,
+              handleColor: Colors.amberAccent,
+            ),
+            onReady: () {
+              model.controller.addListener(model.listener);
+            },
+          ),
+          builder: (context, player) => Scaffold(
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(50.0.h),
+              child: AppBar(
+                centerTitle: true,
+                title: Text(title),
+                titleTextStyle: Theme.of(context).textTheme.headline3,
+              ),
+            ),
+            body: Center(
+              child: ListView(
+                children: [
+                  player,
+                  SizedBox(height: 24.0.h),
+                  Text(
+                    args.youtubeVideo.title,
+                    style: Theme.of(context).textTheme.headline4,
                   ),
-                  onReady: () {
-                    model.controller.addListener(model.listener);
-                  },
-                ),
-                builder: (context, player) {
-                  return player;
-                },
-              );
-            }),
-            SizedBox(height: 24.0.h),
-            Text(
-              args.youtubeVideo.title,
-              style: Theme.of(context).textTheme.headline4,
+                  SizedBox(height: 24.0.h),
+                  Text(
+                    args.youtubeVideo.description.toString(),
+                    style: Theme.of(context).textTheme.headline2,
+                  ),
+                ],
+              ),
             ),
-            SizedBox(height: 24.0.h),
-            Text(
-              args.youtubeVideo.description.toString(),
-              style: Theme.of(context).textTheme.headline2,
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
