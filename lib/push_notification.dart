4b8a413083f04fcdb30bb027a3fde1ed6807c60b
curite <onthepound@gmail.com>
2022-03-29 15:57:06 +0900

    FlutterLocalNotificationsPluginを定義

--- lib/push_notification.dart
+++ lib/push_notification.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class PushNotification extends ChangeNotifier {
-
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 }
