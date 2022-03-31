1a3928028f67e500630dc826a18f28cb4bb8c492
curite <onthepound@gmail.com>
2022-03-31 16:33:04 +0900

    アプリがバックグラウンドで起動中に通知が届き、通知をタップしたら中の処理が呼び出される

--- lib/push_notification_model.dart
+++ lib/push_notification_model.dart
@@ -5,12 +5,22 @@ class PushNotificationModel {
 
     FirebaseMessaging.instance.getInitialMessage();
 
-    ///foreground work
+    ///アプリがフォアグランドにある時に呼ばれる
+    ///アプリがバックグラウンドにある時に通知を受信すると
+    ///端末に通知は届くが、以下の処理は呼ばれない
     FirebaseMessaging.onMessage.listen((message) {
       if(message.notification != null) {
         print(message.notification!.body);
         print(message.notification!.title);
       }
+    });
+
+    ///アプリがバックグラウンドで起動中（アプリが終了していない状態）で
+    ///通知をタップすると呼び出される
+    ///アプリがフォアグランドにある時は通知が来ない
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      final routeFromMessage = message.data['route'];
+      print(routeFromMessage);
     });
   }
 }
