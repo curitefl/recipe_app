68c4cc3ae8c56858cf201c5d07317038a5441ad8
curite <onthepound@gmail.com>
2022-04-04 17:15:55 +0900

    アプリバージョンで比較できるよう対応

--- lib/version_check_service.dart
+++ lib/version_check_service.dart
@@ -1,12 +1,13 @@
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:version/version.dart';
 
 class VersionCheckService {
   static const String configName = 'force_update_app_version';
 
   Future<bool> versionCheck() async {
     final PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    int currentVersion = int.parse(packageInfo.buildNumber);
+    Version currentVersion = Version.parse(packageInfo.version);
     final FirebaseRemoteConfig firebaseRemoteConfig = FirebaseRemoteConfig.instance;
 
     try {
@@ -15,15 +16,15 @@ class VersionCheckService {
           minimumFetchInterval: Duration.zero),
       );
       await firebaseRemoteConfig.fetchAndActivate();
-      int newVersion = firebaseRemoteConfig.getInt(configName);
+      Version newVersion = Version.parse(firebaseRemoteConfig.getString(configName));
       if (newVersion > currentVersion) {
-        print('ビルドバージョンが古いです');
+        print('アプリバージョンが古いです');
         return true;
       }
     } catch (exception) {
       print('リモート設定をフェッチできません。キャッシュされた値またはデフォルト値が使用されます。');
     }
-    print('ビルドバージョンは最新です');
+    print('アプリバージョンは最新です');
     return false;
   }
 }
